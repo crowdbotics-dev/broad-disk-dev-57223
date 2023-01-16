31f3ef54cfcdce3d979cf76e33707bcac8f2dd07
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 10:28:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/broad_disk_dev_57223/MainActivity.java
@@ -0,0 +1,15 @@
+package com.broad_disk_dev_57223;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "broad_disk_dev_57223";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-disk-dev-57223",
+  url: "https://broad-disk-dev-57223.botics.co",
+  api: "https://broad-disk-dev-57223.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
